c0c5b9a5819d9c683622be41f0bbf27d6b766542
Gang Wu <gokure@gmail.com>
2021-05-24 19:26:57 +0800

    修复 swoole-4.6.7 之前版本对开启CURL Hooks的兼容
    - swoole-4.5.0 支持部分 curl hook
    - swoole-4.6.0 之后支持 native-curl hook，但存在问题
    - swoole-4.6.7 修复 Symfony HttpClient 使用 native curl 的问题 (#4208)

--- src/SentryExceptionHandler.php
+++ src/SentryExceptionHandler.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Minbaby\HyperfSentry;
 
+use Closure;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Sentry\ClientBuilderInterface;
 use Sentry\FlushableClientInterface;
+use Swoole\Runtime;
 use Throwable;
 
 class SentryExceptionHandler extends ExceptionHandler
@@ -27,17 +29,19 @@ class SentryExceptionHandler extends ExceptionHandler
      */
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
-        $clientBuilder = $this->container->get(ClientBuilderInterface::class);
+        $this->wrapCurlHooks(function () use ($throwable) {
+            $clientBuilder = $this->container->get(ClientBuilderInterface::class);
 
-        SentryContext::getHub();
+            SentryContext::getHub();
 
-        $config = $this->container->get(ConfigInterface::class);
+            $config = $this->container->get(ConfigInterface::class);
 
-        SentryContext::getHub()->captureException($throwable);
+            SentryContext::getHub()->captureException($throwable);
 
-        if (($client = $clientBuilder->getClient()) instanceof FlushableClientInterface) {
-            $client->flush((int) $config->get('sentry.flush_timeout', 2));
-        }
+            if (($client = $clientBuilder->getClient()) instanceof FlushableClientInterface) {
+                $client->flush((int)$config->get('sentry.flush_timeout', 2));
+            }
+        });
 
         return $response;
     }
@@ -52,5 +56,47 @@ class SentryExceptionHandler extends ExceptionHandler
     public function isValid(Throwable $throwable): bool
     {
         return true;
+    }
+
+    /**
+     * Wrap CURL Hooks
+     *
+     *  swoole 4.6.7 之前关闭 SWOOLE_HOOK_CURL/SWOOLE_HOOK_NATIVE_CURL
+     *
+     *   - swoole-4.5.0 支持部分 curl hook
+     *   - swoole-4.6.0 之后支持 native-curl hook，但存在问题
+     *   - swoole-4.6.7 修复 Symfony HttpClient 使用 native curl 的问题 (#4208)
+     *
+     * @param Closure $callback
+     * @return mixed
+     */
+    protected function wrapCurlHooks(Closure $callback)
+    {
+        // Symfony\Component\HttpClient\CurlHttpClient required.
+        if (!defined('CURLHEADER_SEPARATE')) {
+            define('CURLHEADER_SEPARATE', 1);
+        }
+
+        $symfonyHttpClientSupports = !function_exists('swoole_version') || version_compare(swoole_version(), '4.6.7') >= 0;
+        if ($symfonyHttpClientSupports) {
+            return $callback();
+        }
+
+        $curlFlags = $hookFlags = Runtime::getHookFlags() ?? SWOOLE_HOOK_ALL;
+
+        if (defined('SWOOLE_HOOK_CURL') && ($hookFlags & SWOOLE_HOOK_CURL)) {
+            $curlFlags ^= SWOOLE_HOOK_CURL;
+        }
+
+        if (defined('SWOOLE_HOOK_NATIVE_CURL') && ($hookFlags & SWOOLE_HOOK_NATIVE_CURL)) {
+            $curlFlags ^= SWOOLE_HOOK_NATIVE_CURL;
+        }
+
+        try {
+            Runtime::enableCoroutine(true, $curlFlags);
+            return $callback();
+        } finally {
+            Runtime::enableCoroutine(true, $hookFlags);
+        }
     }
 }
